a7eb21cd4d5150b2271b990d57df761c35cb8ac2
bkr <composer22@hotmail.com>
2012-01-11 10:30:50 -0800

    Added Math class for additional methods beyond core Ruby.  No monkey patching or childing

--- lib/miltons_machine.rb
+++ lib/miltons_machine.rb
@@ -7,14 +7,21 @@
 #
 #   require 'miltons_machine'
 #
+
+# Base
 require 'miltons_machine/core/constants'
+require 'miltons_machine/core/math'
+
+# Core classes
 require 'miltons_machine/core/forte_set'
 require 'miltons_machine/core/forte_dictionary'
 require 'miltons_machine/core/tuning'
 require 'miltons_machine/core/spectrum'
 
+# Tools
 require 'miltons_machine/tools/generator'
 require 'miltons_machine/tools/matrix_analyzer'
 
+# Meta
 require 'miltons_machine/testing/helpers'
 require 'miltons_machine/version'
